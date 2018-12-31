d09db9ffb93094a860083a2b8f85a925407928cf
Jonas Silva <jonas.silva@farfetch.com>
2018-12-31 13:39:15 +0000

    adicionando o projeto DTO

---
+++ Src/Application.DTO/Filme.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.DTO
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class FilmeDTO
+    {
+        public string Nome { get; set; }
+
+        public DateTime InicioEmCartaz { get; set; }
+
+        public DateTime FimEmCartaz { get; set; }
+
+        public List<Sessao> Sessoes { get; set; }
+
+        public List<Sala> Salas { get; set; }
+    }
+}
