a7d9de991f685c16d01738aa7c9f489401f4b526
Jonas Silva <jonas.silva@farfetch.com>
2018-12-31 13:20:50 +0000

    Adicionando arquivos ao repositório.

---
+++ Src/Data.Ingresso/IngressoService.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace Data.Ingresso
+{
+    public class IngressoService
+    {
+        
+    }
+}
---
+++ Src/Ingresso.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ingresso.Application
+{
+    public class Class1
+    {
+    }
+}
